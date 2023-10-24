126319a6c33ef791ae135d302a74b751bcd1aa39
Muggle Wei <mugglewei@gmail.com>
2023-10-24 17:51:02 +0800

    support meson build

---
+++ build_with_meson.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+origin_dir="$(dirname "$(readlink -f "$0")")"
+build_dir=$origin_dir/build
+dist_dir=$origin_dir/dist
+
+cd $origin_dir
+rm -rf $build_dir
+meson setup $build_dir
+
+meson compile -C $build_dir
